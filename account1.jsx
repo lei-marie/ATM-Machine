0f4d78dcfe83570e59b9bbac42058312222b58f4
Lei Marie Lafaele <llafaele.murphy@outlook.com>
2022-05-31 10:06:26 -0400

    Updated

--- account1.jsx
+++ account1.jsx
@@ -1,37 +1,47 @@
 // this keeps a running total of deposits and withdrawals\
-const ATMDeposit = ({ onChange, isDeposit }) => {
+const ATMDeposit = ({ onChange, isDeposit}) => {
   const choice = ["Deposit", "Withdrawal"];
   return (
     <label className="label huge">
       <h3> {choice[Number(!isDeposit)]}</h3>
       <input type="number" onChange={onChange}></input>
-      <input type="submit" value="Submit"></input>
+      <input type="submit"  value="Submit"></input>
     </label>
   );
 };
 const Account = () => {
+  const [deposit, setDeposit] = React.useState(0);
   const [accountState, setAccountState] = React.useState(0);
   const [isDeposit, setIsDeposit] = React.useState(true);
-  let  deposit = 0;
+  const [validTransaction, setValidTransaction] = React.useState(false);
 
   let status = `Account Balance $ ${accountState}`;
 
   const handleChange = event => {
-    deposit = Number(event.target.value);
+    if(Number(event.target.value <= 0)) {
+      return setValidTransaction(false);
+    } 
+    if (Number(event.target.value) > accountState) {
+      setValidTransaction(false);
+    } else {
+      setValidTransaction(true);
+    };
+    setDeposit(Number(event.target.value));
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (event) => {
     let newTotal = isDeposit ? accountState + deposit : accountState - deposit;
     setAccountState(newTotal);
+    setValidTransaction(false);
     event.preventDefault();
   };
 
   return (
     <form onSubmit={handleSubmit} id="form">
       <h2 id="total">{status}</h2>
-      <button onClick={()=>setIsDeposit(true)}>Deposit</button>
-      <button onClick={()=>setIsDeposit(false)}>Withdrawal</button>
-      <ATMDeposit onChange={handleChange} isDeposit={isDeposit}> Deposit</ATMDeposit>
+      <button onClick={()=>setIsDeposit(true)} value="Deposit">Deposit</button>
+      <button onClick={()=>setIsDeposit(false)} value="Withdrawal">Withdrawal</button>
+      <ATMDeposit onChange={handleChange} isDeposit={isDeposit} isValid={validTransaction}> Deposit</ATMDeposit>
     </form>
   );
 };
